f6dffdc4e8c9c922f07057c0eb904a8057498106
sumaautomationLK <sumanga0000@gmail.com>
2025-01-01 15:43:20 +0530

    addind

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
 import './index.css';
 import App from './App';
 import { WebSocketProvider } from './Components/Common/WebSocketContext'; // Import WebSocket provider
@@ -7,12 +10,12 @@ import { WebSocketProvider } from './Components/Common/WebSocketContext'; // Imp
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  {/* Waraping websocket provider */}
-  <WebSocketProvider>
-  <App />
-  </WebSocketProvider>
-  
-</React.StrictMode>
+    {/* Waraping websocket provider */}
+    <WebSocketProvider>
+    <App />
+    </WebSocketProvider>
+    
+  </React.StrictMode>
 );
 
 
