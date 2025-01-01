d6fcde1980a18c110a37a563ff345306bcc218d4
sumaautomationLK <sumanga0000@gmail.com>
2025-01-01 15:07:09 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import './index.css'
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='text-white'>
      
       Hello
-      
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { WebSocketProvider } from './Components/Common/WebSocketContext'; // Import WebSocket provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  {/* Waraping websocket provider */}
+  <WebSocketProvider>
+  <App />
+  </WebSocketProvider>
+  
+</React.StrictMode>
 );
 
 
