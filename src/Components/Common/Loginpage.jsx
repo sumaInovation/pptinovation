57df313995d669b376a52558314c7310fce2a7b4
sumaautomationLK <sumanga0000@gmail.com>
2025-01-04 11:57:43 +0530

    updates

---
+++ src/Components/Common/Loginpage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const clientID="39253442113-0vh548enso8si0f1pjg27cbensc6qpba.apps.googleusercontent.com";
+
+
+   
+const Loginpage = () => {
+   
+  return (
+    <div id="signInButton">
+      
+      
+    </div>
+  )
+}
+
+
+export default Loginpage
---
+++ src/Components/Common/Logout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const clientID="39253442113-0vh548enso8si0f1pjg27cbensc6qpba.apps.googleusercontent.com";
+
+
+
+const Logout = () => {
+    
+  
+  return (
+    <div id="signInButton">
+    
+      
+    </div>
+  )
+}
+
+
+export default Logout
