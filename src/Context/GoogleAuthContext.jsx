70927096d7de2cd4f15050e4084598ccfd201950
sumaautomationLK <sumanga0000@gmail.com>
2025-01-07 12:55:57 +0530

    Update GoogleAuthContext.jsx

--- src/Context/GoogleAuthContext.jsx
+++ src/Context/GoogleAuthContext.jsx
@@ -58,7 +58,7 @@ export const GoogleAuthProvider = ({ children }) => {
         if(data.error==undefined)
          setUserData(data);
         window.location.reload();
-        alert(data)
+       
 
       })
       .catch((err) => {console.error("Error fetching profile:", err)
