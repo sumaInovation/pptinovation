feba033909ed9e6ea3561bb4debc62c0f5649b19
sumaautomationLK <sumanga0000@gmail.com>
2025-01-08 18:49:35 +0530

    up

--- src/Context/GoogleAuthContext.jsx
+++ src/Context/GoogleAuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 import { jwtDecode } from 'jwt-decode'
 import axios from 'axios';
-import { em } from "framer-motion/client";
+import Cookies from 'js-cookie';
 export const GoogleAuthContext = createContext();
 
 export const GoogleAuthProvider = ({ children }) => {
@@ -11,12 +11,12 @@ export const GoogleAuthProvider = ({ children }) => {
 
 
   const handleLoginSuccess = async (response) => {
-    const idToken ="123" // Google ID Token
+    
     
        const decodedata=jwtDecode(response.credential);
        const{name,email,picture}=decodedata;
        const user={name,email,picture}
-       console.log(user)
+       
       try {
         
   
@@ -29,8 +29,22 @@ export const GoogleAuthProvider = ({ children }) => {
       } catch (error) {
         console.error('Error sending POST request:', error);
       }
-    
+       
   
+   }
+   const getProfiledta=async()=>{
+    const cookieValue = Cookies.get('token');
+       try{
+    const res = await axios.post('https://googlesheet-yuetcisb.b4a.run/profile', cookieValue,{
+      withCredentials: true, // Send cookies or credentials with the request
+      headers: {
+        'Content-Type': 'application/json',
+      }});
+      console.log(res);
+  } catch (error) {
+    console.error('Error sending POST request:', error);
+  }
+
    }
   const handleLogout = async () => {
     axios.get('https://googlesheet-yuetcisb.b4a.run/deleteCookie',{ withCredentials: true }).then((res) =>{
